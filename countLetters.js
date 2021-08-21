16de2aad8d820340b4771685601f61bf6af8764c
Wahab Alamin <wahabalamin@gmail.com>
2021-08-20 23:00:36 -0400

    created a function that counts the letters within a sentence and returns the answer in an object

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let lettersCounted = {};
+  for (let letters of sentence) {
+    if (lettersCounted[letters]) {
+      lettersCounted[letters] += 1
+    } else {
+      lettersCounted[letters] = 1
+    }
+  }
+
+  return lettersCounted;
+}
