794792b5a4d7e37da08e24f95b48996435725b62
Wahab Alamin <wahabalamin@gmail.com>
2021-08-23 19:58:09 -0400

    updated file to only contain function findKeyByValue and to export it

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(object, value) {
   let key;
   for (let genre in object) {
@@ -17,14 +9,4 @@ const findKeyByValue = function(object, value) {
   return key;
 }
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
-
-
+module.exports = findKeyByValue;
