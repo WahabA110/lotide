027ec902007b7a68b623ed1b45a595bc1b61735b
Wahab Alamin <wahabalamin@gmail.com>
2021-08-23 19:04:10 -0400

    updated file to use mocha and chai to test results. Using describe and it functions to test various results instead of the previous assertEqual function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,35 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const empty = [];
-const word = ["Yo Yo"];
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
+describe ('#tail', () => {
+  it('returns 0 for [].length', () => {
+    assert.deepEqual(tail([]).length, 0);
+  });
 
-assertEqual(tail(empty).length, 0);
-assertEqual(tail(empty)[0], undefined);
+  it('returns undefined for [] at [0]', () => {
+    assert.deepEqual(tail([])[0], undefined);
+  });
 
-assertEqual(tail(word).length, 0);
-assertEqual(tail(word)[0], undefined);
+  it('returns 0 for ["Yo Yo"].length', () => {
+    assert.deepEqual(tail(["Yo Yo"]).length, 0);
+  });
 
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words)[1], "Labs");
+  it('returns undefined for ["Yo Yo"] at [0]', () => {
+    assert.deepEqual(tail(['Yo Yo'])[0], undefined);
+  });
+
+  it('returns 2 for ["Yo Yo", "Lighthouse", "Labs"].length', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+  });
+
+  it('returns "Lighthouse" for ["Yo Yo", "Lighthouse", "Labs"] at [0]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+
+  it('returns "Labs" for ["Yo Yo", "Lighthouse", "Labs"] at [1]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[1], "Labs");
+  });
+
+});
