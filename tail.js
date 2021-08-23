0627c18faeae9e40b0ea133eac8809a620bef1cf
Wahab Alamin <wahabalamin@gmail.com>
2021-08-23 15:50:21 -0400

    removed most of the code so file only contains the tail function and exports it for future use

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 function tail(arr) {
   let group = [];
   for (let i = 1; i < arr.length; i ++) {
@@ -15,7 +6,4 @@ function tail(arr) {
   return group;
 }
 
-const words = [];
-const word = ["Yo Yo"];
-console.log(tail(words));
-console.log(tail(word));
+module.exports = tail;
