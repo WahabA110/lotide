e83cff7c639ce73c466e1e04518caabd7d0ad249
Wahab Alamin <wahabalamin@gmail.com>
2021-08-23 17:26:16 -0400

    removed most of code so file only contains a require of eqArrays function, an assertArraysEqual function, and exports the function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function (arrVal1, arrVal2) {
-  if (arrVal1.length !== arrVal2.length) {
-    return false;
-  }
-  for (let i = 0; i < arrVal1.length; i ++) {
-    if (arrVal1[i] !== arrVal2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function (arrVal1, arrVal2) {
 
@@ -20,7 +9,4 @@ const assertArraysEqual = function (arrVal1, arrVal2) {
   );
 };
 
-
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
