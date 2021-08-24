e006f4f0e46f0a0ea97762e23075d1fb662e8e5a
Wahab Alamin <wahabalamin@gmail.com>
2021-08-23 20:00:40 -0400

    created file to require everything we've worked on for the lotide project so far. File also exports them all so we only need to require this file for every function rather than each file seperately

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require('./head');
+const middle = require('./middle');
+const tail = require('./tail');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const without = require('./without');
+
+
+
+module.exports = {
+  head: head,
+  middle: middle,
+  tail: tail,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  without: without,
+}
