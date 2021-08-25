f4c4a39087ff588379083e9ce67cd7253496c2d9
Wahab Alamin <wahabalamin@gmail.com>
2021-08-24 22:54:48 -0400

    updated file to use describe and it functions to test various results and whether they produce the desired outcomes

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,26 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
+describe ('#eqArrays', () => {
+  it('returns true for [],[]', () => {
+    assert.deepEqual(eqArrays([],[]), true);
+  });
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1, 4]), false) // => false
+  it('returns true for [1],[1]', () => {
+    assert.deepEqual(eqArrays([1],[1]), true);
+  });
+
+  it('returns true for [1, 2, 3],[1, 2, 3]', () => {
+    assert.deepEqual(eqArrays([1, 2, 3],[1, 2, 3]), true);
+  });
+
+  it('returns false for [1, 2],[1, 2, 3]', () => {
+    assert.deepEqual(eqArrays([1, 2],[1, 2, 3]), false);
+  });
+
+  it('returns false for [1, 2],[2, 1]', () => {
+    assert.deepEqual(eqArrays([1, 2],[2, 1]), false);
+  });
+
+});
